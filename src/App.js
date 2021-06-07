eb1df4928c6da653bc6956b625f9bd60fbce1751
Dustin Brinkman <mimirs.spring@gmail.com>
2021-06-07 04:57:29 -0400

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
+  //onlicks
+  const onClickLinkBot = (e) => {
+    e.preventDefault();
+    window.location = "https://fervent-noyce-f7844a.netlify.app/";
+  };
+
+  const onClickCounter = (e) => {
+    e.preventDefault();
+    window.location = "https://counterthatkeepsoncounting.netlify.app/";
+  };
+
+  const onClickFolio = (e) => {
+    e.preventDefault();
+    window.location = "https://upbeat-shirley-6f5013.netlify.app/";
+  };
+
+  const onClickYoutube = (e) => {
+    e.preventDefault();
+    window.location = "https://youtube.com/";
+  };
+
+  const onClickSamurai = (e) => {
+    e.preventDefault();
+    window.location = "https://www.samuraioflegend.com/";
+  };
+
+  const onClickGoogle = (e) => {
+    e.preventDefault();
+    window.location = "https://google.com/";
+  };
+
+  const onClickStack = (e) => {
+    e.preventDefault();
+    window.location = "https://stackoverflow.com/";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='middle-box'>
+        <div className='middle-middle-box'>
+          <h2 className='to-do-title'>To-Do List</h2>
+        </div>
+      </div>
+      <div className="shortcut-container">
+        <br />
+        <div className="button-container">
+          <Button
+            className="button"
+            onClick={onClickLinkBot}
+            variant="contained"
+            color="secondary"
+            size="large"
+          >
+            Link Bot
+          </Button>
+          <p>{/* for some reason br wont work so I'm doing this instead */}</p>
+          <Button
+            className="button"
+            onClick={onClickCounter}
+            variant="contained"
+            color="secondary"
+            size="large"
+          >
+            Counter Game
+          </Button>
+          <p>{/* for some reason br wont work so I'm doing this instead */}</p>
+          <Button
+            className="button"
+            onClick={onClickFolio}
+            variant="contained"
+            color="secondary"
+            size="large"
+          >
+            Folio Site
+          </Button>
+          <p>{/* for some reason br wont work so I'm doing this instead */}</p>
+          <Button
+            className="button"
+            onClick={onClickYoutube}
+            variant="contained"
+            color="secondary"
+            size="large"
+          >
+            Watch Youtube
+          </Button>
+          <p>{/* for some reason br wont work so I'm doing this instead */}</p>
+          <Button
+            className="button"
+            onClick={onClickSamurai}
+            variant="contained"
+            color="secondary"
+            size="large"
+          >
+            Samurai Legend
+          </Button>
+          <p>{/* for some reason br wont work so I'm doing this instead */}</p>
+          <Button
+            className="button"
+            onClick={onClickGoogle}
+            variant="contained"
+            color="secondary"
+            size="large"
+          >
+            Google Stuff
+          </Button>
+          <p>{/* for some reason br wont work so I'm doing this instead */}</p>
+          <Button
+            className="button"
+            onClick={onClickStack}
+            variant="contained"
+            color="secondary"
+            size="large"
+          >
+            Stack Overflow
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
