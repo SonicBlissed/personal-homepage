606edb5afaf3ee9525e1571558bcf7073f66ee48
dustin-brinkman <mimirs.spring@gmail.com>
2021-06-12 21:15:34 -0400

    added edit functionality

--- src/To-do/to-do.js
+++ src/To-do/to-do.js
@@ -2,49 +2,83 @@ import "./to-do.css";
 import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import Fade from "react-reveal/Fade";
-
-const initialState = false;
+import Lockr from 'lockr';
 
 function Todo() {
-  const [done1, setDone1] = useState(initialState);
-  const [done2, setDone2] = useState(initialState);
-  const [done3, setDone3] = useState(initialState);
+  // const initialState = false;
+  const [done1, setDone1] = useState(false);
+  const [done2, setDone2] = useState(false);
+  const [done3, setDone3] = useState(false);
+  const [button1, setbutton1] = useState(Lockr.get('task1'));
+  const [button2, setbutton2] = useState(Lockr.get('task2'));
+  const [button3, setbutton3] = useState(Lockr.get('task3'));
+  const [edit, setEdit] = useState('edit');
+  
+  const taskArray = [
+    { task: button1, completed: done1 },
+    { task: "Get A Job", completed: done2 },
+    { task: "DB For Clicker", completed: done3 },
+  ];
 
-  const taskArray = ["Finish Project", "Get A Job", "DB For Clicker"];
+ function handleInputTask1(evt) {
+    setbutton1(evt.target.value)
+}
+
+function handleInputTask2(evt) {
+    setbutton2(evt.target.value)
+}
+
+function handleInputTask3(evt) {
+    setbutton3(evt.target.value)
+}
 
 
- function disabledHelper1() {
-   if (done1 === true){
-       setDone1(false)
-   } else {
-       setDone1(true)
-   }
- }
- function disabledHelper2() {
-    if (done2 === true){
-        setDone2(false)
+  function disabledHelper1() {
+    if (done1 === false) {
+      setDone1(true);
     } else {
-        setDone2(true)
+      setDone1(false);
+    }
+  }
+  function disabledHelper2() {
+    if (done2 === true) {
+      setDone2(false);
+    } else {
+      setDone2(true);
     }
   }
   function disabledHelper3() {
-    if (done3 === true){
-        setDone3(false)
+    if (done3 === true) {
+      setDone3(false);
     } else {
-        setDone3(true)
+      setDone3(true);
     }
   }
   function resetHelper() {
-      if (done1 === true || done2 === true || done3 === true){
-          setDone1(false)
-          setDone2(false)
-          setDone3(false)
+    if (done1 === true || done2 === true || done3 === true) {
+      setDone1(false);
+      setDone2(false);
+      setDone3(false);
+    } else {
+      return null;
+    }
+  }
+
+  function editHelper () {
+      if (edit === 'edit') {
+          setEdit('editting')
       } else {
-          return null
+          setEdit('edit')
+          Lockr.set('task1', button1); 
+          Lockr.set('task2', button2);
+          Lockr.set('task3', button3);
       }
   }
 
-
+  //   function task1(string) {
+  //     taskArray.shift();
+  //     taskArray.unshift(string);
+  //   }
 
   return (
     <div className="box">
@@ -59,7 +93,7 @@ function Todo() {
             size="large"
             disabled={done1}
           >
-            {taskArray[0]}
+            {Lockr.get('task1')}
           </Button>
           <Button
             className="button"
@@ -69,7 +103,7 @@ function Todo() {
             size="large"
             disabled={done2}
           >
-            {taskArray[1]}
+            {Lockr.get('task2')}
           </Button>
           <Button
             className="button"
@@ -79,27 +113,39 @@ function Todo() {
             size="large"
             disabled={done3}
           >
-            {taskArray[2]}
+            {Lockr.get('task3')}
           </Button>
+        </div>
+      </Fade>
 
-        </div> </Fade>
-
-        <Fade bottom delay={400} cascade distance={"20%"}>
-
-        <div className='bottom-box'>
-        <Button
+      <Fade bottom delay={400} cascade distance={"20%"}>
+        <div className="bottom-box">
+          <Button
             className="button"
             onClick={resetHelper}
             variant="contained"
             color="secondary"
             size="large"
-            
-            >
+          >
             Reset List
           </Button>
+          <Button
+            className="button"
+            onClick={editHelper}
+            variant="contained"
+            color="secondary"
+            size="large"
+          >
+            Edit List
+          </Button>
+
+          <form className={edit}>
+              <input placeholder={button1} type='text' onChange={handleInputTask1}/>
+              <input placeholder={button2} type='text' onChange={handleInputTask2}/>
+              <input placeholder={button3} type='text' onChange={handleInputTask3}/>
+          </form>
         </div>
-              </Fade>
-     
+      </Fade>
     </div>
   );
 }
